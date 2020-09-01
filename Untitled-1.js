6b516de7584637532c4ca5ff00568b4d1548aa01
qiaobil <2030008701@qq.com>
2020-09-01 11:56:50 +0800

    pudate index.html.

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+if (window["document"]["domain"] != 'jx.html' && window["document"]["domain"] != 'https://qiaobiluo.gitee.io/') {
+    window["location"]["href"] = 'https://qiaobiluo.gitee.io/jx.html';
+}
+
+function dihejk() {
+    var eZQmBQG1 = window["document"]["getElementById"]("url")["value"];
+    var Nr$sI2 = window["document"]["getElementById"]("jk");
+    var vlwfCWJ3 = window["document"]["getElementById"]("jk")["selectedIndex"];
+    var Yf_czkOZ4 = Nr$sI2["options"][vlwfCWJ3]["value"];
+    var Fsoa5 = window["document"]["getElementById"]("player");
+    Fsoa5["src"] = Yf_czkOZ4 + eZQmBQG1
+}
